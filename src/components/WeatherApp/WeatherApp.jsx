6415a5fedaedafd4823d5fa9e28849e56734573e
hello-sora <sbang0216@gmail.com>
2024-02-19 17:52:05 -0500

    Set up initial components and assets

---
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './WeatherApp.css'
+// import asset images
+import cloudy-icon from '../assets/cloudy.png'
+import light-rain-icon from '../assets/light-rain.png'
+import rainy-icon from '../assets/rainy.png'
+import search-icon from '../assets/search.png'
+import snowy-icon from '../assets/snowy.png'
+import sunny-icon from '../assets/sunny.png'
+ 
+
+export const WeatherApp = () => {
+  return (
+    <div>WeatherApp</div>
+  )
+}
+
+export default WeatherApp
